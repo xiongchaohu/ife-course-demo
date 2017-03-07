070541e0c337068dd97849a744dbe6e0252902ac
hxc <hxc>
2017-03-07 10:36:28 +0800

    channge task_4.js to add some action

--- binbin/task_4/task_4.js
+++ binbin/task_4/task_4.js
@@ -11,31 +11,54 @@ var rightOut = document.getElementById("rightOut");
 //点击左侧入按钮添加事件
 leftIn.onclick = function () {
     var oLi = document.createElement("li");
-    oLi.innerHTML = oInput.value;
-    oUl.insertBefore(oLi, oUl.firstChild);
+    var inputValue = oInput.value;
+
+    if (/^[0-9]+$/.test(inputValue)) {
+        oLi.innerHTML = inputValue;
+        oUl.insertBefore(oLi, oUl.firstChild);
+    } else {
+        alert("请输入数字");
+    }
+
 };
 //点击右侧入事件处理
 rightIn.onclick = function () {
     var oLi = document.createElement("li");
-    oLi.innerHTML = oInput.value;
-    oUl.appendChild(oLi);
+    var inputValue = oInput.value;
+
+    if (/^[0-9]+$/.test(inputValue)) {
+        oLi.innerHTML = inputValue;
+        oUl.appendChild(oLi);
+    } else {
+        alert("请输入数字");
+    }
 };
 
 //点击左侧出
 leftOut.onclick = function () {
-    var temp = oUl.firstChild;
+    var temp = oUl.firstElementChild;
     if (temp.innerHTML) {
         oUl.removeChild(temp);
-        alert(temp.innerHTML);
+        alert("删除的元素是：" + temp.innerHTML);
+    } else {
+        alert("没有可删除的元素");
     }
 
 };
 //点击右侧出
 rightOut.onclick = function () {
-    var temp = oUl.lastChild;
+    var temp = oUl.lastElementChild;
     if (temp.innerHTML) {
-        oUl.removeChild(oUl.lastChild);
-        alert(temp.innerHTML);
+        oUl.removeChild(temp);
+        alert("删除的元素是：" + temp.innerHTML);
+    } else {
+        alert("没有可删除的元素");
     }
 
 };
+//点击li删除相应元素
+oUl.onclick = function (event) {
+    var ev = event.target || event.srcElement;
+    oUl.removeChild(ev);
+}
+
